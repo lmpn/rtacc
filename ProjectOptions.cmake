2ec6df41c3c7ba3c8b581f943406bc23fd56735d
Template Janitor <template.janitor@example.com>
2023-07-17 11:06:13 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -4,7 +4,7 @@ include(CMakeDependentOption)
 include(CheckCXXCompilerFlag)
 
 
-macro(myproject_supports_sanitizers)
+macro(rtacc_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
     set(SUPPORTS_UBSAN ON)
   else()
@@ -18,183 +18,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(rtacc_setup_options)
+  option(rtacc_ENABLE_HARDENING "Enable hardening" ON)
+  option(rtacc_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    rtacc_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    rtacc_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  rtacc_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR rtacc_PACKAGING_MAINTAINER_MODE)
+    option(rtacc_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(rtacc_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(rtacc_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(rtacc_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(rtacc_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(rtacc_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(rtacc_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(rtacc_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(rtacc_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(rtacc_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(rtacc_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(rtacc_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(rtacc_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(rtacc_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(rtacc_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(rtacc_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(rtacc_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(rtacc_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(rtacc_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(rtacc_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(rtacc_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(rtacc_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(rtacc_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(rtacc_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(rtacc_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(rtacc_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      rtacc_ENABLE_IPO
+      rtacc_WARNINGS_AS_ERRORS
+      rtacc_ENABLE_USER_LINKER
+      rtacc_ENABLE_SANITIZER_ADDRESS
+      rtacc_ENABLE_SANITIZER_LEAK
+      rtacc_ENABLE_SANITIZER_UNDEFINED
+      rtacc_ENABLE_SANITIZER_THREAD
+      rtacc_ENABLE_SANITIZER_MEMORY
+      rtacc_ENABLE_UNITY_BUILD
+      rtacc_ENABLE_CLANG_TIDY
+      rtacc_ENABLE_CPPCHECK
+      rtacc_ENABLE_COVERAGE
+      rtacc_ENABLE_PCH
+      rtacc_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  rtacc_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (rtacc_ENABLE_SANITIZER_ADDRESS OR rtacc_ENABLE_SANITIZER_THREAD OR rtacc_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(rtacc_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(rtacc_global_options)
+  if(rtacc_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    rtacc_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  rtacc_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(rtacc_ENABLE_HARDENING AND rtacc_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR rtacc_ENABLE_SANITIZER_UNDEFINED
+       OR rtacc_ENABLE_SANITIZER_ADDRESS
+       OR rtacc_ENABLE_SANITIZER_THREAD
+       OR rtacc_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${rtacc_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${rtacc_ENABLE_SANITIZER_UNDEFINED}")
+    rtacc_enable_hardening(rtacc_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(rtacc_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(rtacc_warnings INTERFACE)
+  add_library(rtacc_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  rtacc_set_project_warnings(
+    rtacc_warnings
+    ${rtacc_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(rtacc_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    configure_linker(myproject_options)
+    configure_linker(rtacc_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  rtacc_enable_sanitizers(
+    rtacc_options
+    ${rtacc_ENABLE_SANITIZER_ADDRESS}
+    ${rtacc_ENABLE_SANITIZER_LEAK}
+    ${rtacc_ENABLE_SANITIZER_UNDEFINED}
+    ${rtacc_ENABLE_SANITIZER_THREAD}
+    ${rtacc_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(rtacc_options PROPERTIES UNITY_BUILD ${rtacc_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(rtacc_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      rtacc_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(rtacc_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    rtacc_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(rtacc_ENABLE_CLANG_TIDY)
+    rtacc_enable_clang_tidy(rtacc_options ${rtacc_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(rtacc_ENABLE_CPPCHECK)
+    rtacc_enable_cppcheck(${rtacc_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(rtacc_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    rtacc_enable_coverage(rtacc_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(rtacc_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(rtacc_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(rtacc_ENABLE_HARDENING AND NOT rtacc_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR rtacc_ENABLE_SANITIZER_UNDEFINED
+       OR rtacc_ENABLE_SANITIZER_ADDRESS
+       OR rtacc_ENABLE_SANITIZER_THREAD
+       OR rtacc_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    rtacc_enable_hardening(rtacc_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
