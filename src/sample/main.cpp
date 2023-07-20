775ac0386f7e27bd9807bf915922d72b763a440b
Luis Neto <luis.neto.dev@gmail.com>
2023-07-20 09:14:46 +0100

    wip

--- src/sample/main.cpp
+++ src/sample/main.cpp
@@ -1,27 +1,59 @@
+#include <CLI/CLI.hpp>
 #include <array>
 #include <functional>
 #include <iostream>
 #include <optional>
-#include <CLI/CLI.hpp>
 #include <spdlog/spdlog.h>
 
 
 // This file will be generated automatically when cur_you run the CMake
 // configuration step. It creates a namespace called `rtacc`. You can modify
 // the source template at `configured_files/config.hpp.in`.
+#include <asio/io_context.hpp>
+#include <asio/posix/stream_descriptor.hpp>
+#include <asio/stream_file.hpp>
 #include <internal_use_only/config.hpp>
+#include <stdio.h>
 
-int cli_main(int argc, const char **argv)
+
+class file
+{
+private:
+  using file_impl = std::FILE *;
+  using file_impl_ptr = std::shared_ptr<file_impl>;
+  file_impl_ptr m_file;
+  file(const file &) = delete;
+  file &operator=(const file &) = delete;
+
+  static create_file_ptr(const std::string &path, const std::string &mode = std::string("r"))
+  {
+    auto fp = std::fopen(path, mode);
+    return fp ? std::shared_ptr<std::FILE>(fp, std::fclose) : std::shared_ptr<std::FILE>();
+  }
+
+public:
+  explicit file(const std::string &path, const std::string &mode = std::string("r"))
+    : m_file(create_file_ptr(path, mode))
+  {}
+
+  auto desc() const { return m_file->_file; }
+
+  ~file()
+  {
+    if (m_file) { std::fclose(m_file); }
+  }
+};
+
+
+int main(int argc, const char **argv)
 {
   try {
     CLI::App app{ fmt::format("{} version {}", rtacc::cmake::project_name, rtacc::cmake::project_version) };
 
-    std::optional<std::string> message;
-    app.add_option("-m,--message", message, "A message to print back out");
+    std::optional<std::string> file_path;
+    app.add_option("-f,--file", file_path, "An input file");
     bool show_version = false;
     app.add_flag("--version", show_version, "Show version information");
-
-   
 
 
     CLI11_PARSE(app, argc, argv);
@@ -31,58 +63,17 @@ int cli_main(int argc, const char **argv)
       return EXIT_SUCCESS;
     }
 
+    constexpr std::size_t size = 1024;
+    asio::io_context ioc{ 1 };
+    file fd_ = file(file_path->c_str(), "r");
+    auto fd_ptr = std::make_shared<FILE>() asio::posix::stream_descriptor stream_(ioc, fd_);
+    std::string buffer_;
+    buffer_.resize(size);
+
+    fclose(fd_);
+
   } catch (const std::exception &e) {
     spdlog::error("Unhandled exception in main: {}", e.what());
   }
   return 0;
-}
-
-//
-// main.cpp
-// ~~~~~~~~
-//
-// Copyright (c) 2003-2023 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
-#include "line_reader.hpp"
-#include "sleep.hpp"
-#include "stdin_line_reader.hpp"
-#include "buffered_file_ingestor.hpp"
-#include <asio/co_spawn.hpp>
-#include <asio/detached.hpp>
-#include <asio/io_context.hpp>
-#include <asio/use_awaitable.hpp>
-#include <iostream>
-
-asio::awaitable<void> do_read(line_reader& reader)
-{
-  for (int i = 0; i < 10; ++i)
-  {
-    auto value = co_await reader.async_read_line("Enter something: ", asio::use_awaitable);
-    std::cout << value ;
-    co_await async_sleep(co_await asio::this_coro::executor, std::chrono::seconds(1), asio::use_awaitable);
-  }
-}
-
-asio::awaitable<void> read_file(buffered_file_ingestor& reader)
-{
-
-  std::string buffer; 
-  do
-  {
-    buffer = co_await reader.read(asio::use_awaitable);
-    std::cout << buffer;
-  } while (true);
-  
-}
-
-int main()
-{
-  asio::io_context ctx{1};
-  buffered_file_ingestor bfi{ctx.get_executor(), std::string("/Users/mindera/repositories/rtacc/README.md")};
-  
-  ctx.run();
 }
